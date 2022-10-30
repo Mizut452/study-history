303110addc91af60f1feaad58f8e64cae2a96bfd
M1zut4 <ssvror444@gmail.com>
2022-10-30 22:16:05 +0900

    アカウント作成ページの設定

--- src/main/java/Mizut452/studyhistory/HomeController.java
+++ src/main/java/Mizut452/studyhistory/HomeController.java
@@ -1,6 +1,7 @@
 package Mizut452.studyhistory;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,6 +10,11 @@ public class HomeController {
     @RequestMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/createaccount")
+    public String createAccount() {
+        return "createAccount";
     }
 
 }
