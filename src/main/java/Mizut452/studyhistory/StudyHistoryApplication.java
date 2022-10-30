c900b186b2bc867ec1e2bd178fd54d76fe9a9d92
M1zut4 <ssvror444@gmail.com>
2022-10-30 21:28:48 +0900

    Initial commit

---
+++ src/main/java/Mizut452/studyhistory/StudyHistoryApplication.java
@@ -0,0 +1,13 @@
+package Mizut452.studyhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyHistoryApplication.class, args);
+	}
+
+}
