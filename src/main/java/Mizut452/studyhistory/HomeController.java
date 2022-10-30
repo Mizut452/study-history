1d6a48886207e9bc796892fd1ed347162bfb0d40
M1zut4 <ssvror444@gmail.com>
2022-10-30 23:40:39 +0900

    UserListDaoの作成

--- src/main/java/Mizut452/studyhistory/HomeController.java
+++ src/main/java/Mizut452/studyhistory/HomeController.java
@@ -1,15 +1,28 @@
 package Mizut452.studyhistory;
 
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Controller
 public class HomeController {
+    record UserList(String id, String mailaddress, String username, String password) {}
+    private List<UserList> userLists = new ArrayList<>();
+    private UserListDao dao;
+
+    @Autowired
+    HomeController(UserListDao dao) {
+        this.dao =dao;
+    }
 
     @RequestMapping("/")
     public String index() {
@@ -17,7 +30,9 @@ public class HomeController {
     }
 
     @GetMapping("/createaccount")
-    public String createAccount() {
+    public String createAccount(Model model) {
+        List<UserList> userLists = dao.findAll();
+        model.addAttribute("userList", userLists);
         return "createAccount";
     }
 
--- src/main/java/Mizut452/studyhistory/UserListDao.java
+++ src/main/java/Mizut452/studyhistory/UserListDao.java
@@ -1,5 +1,7 @@
 package Mizut452.studyhistory;
 
+import Mizut452.studyhistory.HomeController.UserList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.stream.Collectors.toList;
+
 @Service
 public class UserListDao {
     private final JdbcTemplate jdbcTemplate;
@@ -19,23 +23,26 @@ public class UserListDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void create() {
-        SqlParameterSource param = new BeanPropertySqlParameterSource();
+    public void create(UserList userList) {
+        SqlParameterSource param = new BeanPropertySqlParameterSource(userList);
         SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate)
                 .withTableName("userlist");
         insert.execute(param);
     }
 
-    List<Map<String, Object>> result = jdbcTemplate.queryForList(query);
-    List<UserList> userLists = result.stream()
-            .map((Map<String, Object>) row) -> new UserList(
-                    row.get("id").toString();
-                    row.get("mailaddress").toString();
-                    row.get("username").toString();
-                    row.get("password").toString();
+    public List<UserList> findAll() {
+        String query = "SELECT * FROM userlist";
+
+        List<Map<String, Object>> result = jdbcTemplate.queryForList(query);
+        List<UserList> userLists = result.stream()
+                .map((Map<String, Object> row) -> new UserList(
+                    row.get("id").toString(),
+                    row.get("mailaddress").toString(),
+                    row.get("username").toString(),
+                    row.get("password").toString()))
     .toList();
 
-    return userLists;
-    )
+        return userLists;
 
+    }
 }
